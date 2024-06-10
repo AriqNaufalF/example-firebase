63c3886d54904290243e689b3b620855c026b3c2
Ariq Naufal Fadhlurrahman <ariqn55@gmail.com>
2024-06-10 10:19:21 +0700

    log firebase messaging token

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.test.service.createChannel
 import com.example.test.service.requestNotifPermission
 import com.example.test.service.showNotification
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -24,5 +29,10 @@ class MainActivity : AppCompatActivity() {
         createChannel(this)
         // Sample to show notification
         showNotification(this, "App Book", "Hello World!")
+
+        lifecycleScope.launch {
+            // Get firebase messaging token
+            Log.d("MainActivity", "My token: ${FirebaseMessaging.getInstance().token.await()}")
+        }
     }
 }
